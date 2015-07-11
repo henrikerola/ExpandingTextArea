89074bff6f5f856707198edd501097906bcb2099
Henri Kerola <henri.kerola@vaadin.com>
2015-07-11 12:44:32 -0700

    Use Vaadin 7.5.1

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -3,14 +3,12 @@ import sbt.Keys._
 
 object Dependencies {
 
-  val vaadinVersion = "7.4.0.beta2"
+  val vaadinVersion = "7.5.1"
 
   val vaadinServer = "com.vaadin" % "vaadin-server" % vaadinVersion
   val vaadinClient = "com.vaadin" % "vaadin-client" % vaadinVersion
   val vaadinClientCompiler = "com.vaadin" % "vaadin-client-compiler" % vaadinVersion
   val vaadinThemes = "com.vaadin" % "vaadin-themes" % vaadinVersion
-
-  val servletApi = "javax.servlet" % "servlet-api" % "2.5"
 
   val addonDeps = Seq(
     vaadinServer,
@@ -18,7 +16,6 @@ object Dependencies {
   )
 
   val demoDeps = Seq(
-    //servletApi % "provided",
     vaadinClientCompiler % "provided",
     vaadinThemes
   )
